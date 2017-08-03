7cada3ccbf169dfbf87740e468dbeefb65805d5a
Dima Zakharov <dmitriy.zakharov@redspace.com>
2017-08-02 21:43:54 -0300

    Add files via upload

--- clock.py
+++ clock.py
@@ -1,14 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from AddNewCamera import *
 
-sched = getWhoCalled()
+sched = BlockingScheduler() 
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
+	getWhoCalled()
     print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
+	getWhoCalled()
     print('This job is run every weekday at 5pm.')
 
 sched.start()
