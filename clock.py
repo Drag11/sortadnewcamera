b94e8ed7319b50214fc82982aa86e59807fa8468
Dima Zakharov <dmitriy.zakharov@redspace.com>
2017-08-02 21:46:42 -0300

    Add files via upload

--- clock.py
+++ clock.py
@@ -6,11 +6,11 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
 	getWhoCalled()
-    print('This job is run every three minutes.')
+	print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
 	getWhoCalled()
-    print('This job is run every weekday at 5pm.')
+	print('This job is run every weekday at 5pm.')
 
 sched.start()
